9d1f2dc7e0f55c33c1164c3f48878f42c180d941
Kelvin Omozokpia <kelvinjune2@gmail.com>
2024-04-13 20:06:46 +0100

    Js beginner

---
+++ script.js
@@ -0,0 +1,61 @@
+// Javascript
+
+// Introduction
+// Basics
+// Data Types,
+// Operators &
+// Functions
+// Control Structures
+
+// Variables
+// const firstName = 'John';
+// const lastName = 'Doe';
+// let age = 25;
+// let isMarried = false;
+// let school = null;
+
+// const person = {
+//   name: 'John',
+//   age: 25,
+//   isMarried: false,
+//   school: {
+//     name: 'Covenant University',
+//     location: 'Ota, Ogun State',
+//   },
+// };
+
+const wallet = {
+  balance: 0,
+};
+
+const greetPerson = (person) => {
+  person.name = 'Jane';
+  return 'Hello ' + person.name;
+};
+
+console.log(greetPerson(person));
+console.log(person);
+
+// BE(DM)(AS)
+// exponential
+//  (2 + 4) * 4 / 5 ** 2 + 4 - 4 - 5 + 666;
+//  (2 + 4)
+// 5 ** 2;
+
+// Data Types
+
+// Primitive Data Types
+// String;
+// Number;
+// Boolean;
+// Undefined;
+// Null;
+// Object;
+// Symbol;
+
+// symbol is a unique and immutable data type that is often used to identify object properties. A symbol is created using the Symbol() function.
+
+// Secondary Data Types
+// Array;
+// Function;
+// Object;
